65f0c1409b08d8b7e55f6ee60c5bfaa07737ce7f
MehrdadMatin <mehrdad.mpanah@gmail.com>
2022-06-03 15:03:19 -0600

    commit

--- mail/contact.js
+++ mail/contact.js
@@ -1,19 +1,18 @@
 $(function () {
 
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
-        //preventSubmit: false,//true,
+        preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
         submitSuccess: function ($form, event) {
-            //event.preventDefault();
+            event.preventDefault();
             var name = $("input#name").val();
             var email = $("input#email").val();
             var subject = $("input#subject").val();
             var message = $("textarea#message").val();
 
             $this = $("#sendMessageButton");
-            $this.prop("enabled", true);
-            //$this.prop("disabled", true);
+            $this.prop("disabled", true);
 
             $.ajax({
                 url: "contact.php",
@@ -56,7 +55,7 @@ $(function () {
     });
 
     $("a[data-toggle=\"tab\"]").click(function (e) {
-        //e.preventDefault();
+        e.preventDefault();
         $(this).tab("show");
     });
 });
